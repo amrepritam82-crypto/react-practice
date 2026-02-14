a0b85f78d179c3732a4625812c557a03897db6e8
AnkitaAmre <ankita.palande15@gmail.com>
2026-02-14 09:24:51 +0530

    React Basics

---
+++ App.js
@@ -0,0 +1,14 @@
+// const heading= React.createElement("h1", {id:"heading"}, "Hello World");
+// console.log(heading);
+
+const parent = React.createElement("div", {
+    id: "parent"
+}, [React.createElement('div', {
+    id: "child"
+}, "I am child"), React.createElement('div', {
+    id: "child2"
+}, "I am child2")]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(parent);
