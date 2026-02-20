7d037c814f1fe564b28c61735cad4de29071f701
AnkitaAmre <ankita.palande15@gmail.com>
2026-02-20 23:26:46 +0530

    ep-05

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../../data/restaurants";
+import { useState } from "react";
 
 const Body = () => {
+    const [restaurantList, setRestaurantList] = useState(restaurants);
   return (
     <section className="body">
       <div className="search-container">
-        <input type="text" placeholder="Search for restaurants or dishes..." />
-        <button>Search</button>
+        {/* <input type="text" placeholder="Search for restaurants or dishes..." /> */}
+        <button onClick={() => {
+          const filteredList = restaurants.filter(restaurant => restaurant.rating > 4.5);
+          setRestaurantList(filteredList);
+        }}>Top Rated</button>
+        <button onClick={() => setRestaurantList(restaurants)} >Reset</button>
       </div>
       <div className="restaurant-grid">
-        {restaurants.map((restaurant) => (
+        {restaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.id} data={restaurant} />
         ))}
       </div>
